b429debf362b163e0e1de91882ac036a0d3d9867
Rithukrish26 <rithukrish.sjps@gmail.com>
2024-07-01 20:12:20 +0530

    Add files via upload

---
+++ Homework Section.py
@@ -0,0 +1,26 @@
+#18.6.2024
+#name=input("Enter your name")
+#age=int(input('Enter your age'))
+#weight=float(input("Enter your weight"))
+#print(name,age,weight,sep=",")
+#print("Congratulations! You are officially awesome")
+#name=input("Enter your Father's name")
+#age=int(input("Enter your Father's age"))
+#weight=float(input("Enter your Father's weight"))
+#print(name,age,weight,sep=",")
+#print("Congratulations! You are officially awesome")
+#name=input("Enter your Mother's name")
+#age=int(input("Enter your Mother's age"))
+#weight=float(input("Enter your Mother's weight"))
+#print(name,age,weight,sep=",")
+#print("Congratulations! You are officially awesome")
+
+#program to convert celsius to fahrenheit and vise-versa
+celsius = float(input("Enter temperature\
+in celsius: "))
+
+fahrenheit = (celsius * 1.8) + 32
+
+print(str(celsius )+ " degree Celsius\
+is equal to " + str(fahrenheit )+
+	" degree Fahrenheit.")
